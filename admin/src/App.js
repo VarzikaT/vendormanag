875dd303106fd81859be274d24c61f9f75a9d625
Varzika Tiwari <varzikatiwari08@gmail.com>
2022-07-05 17:48:30 +0530

    final changes for the day

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
 import Home from './components/Home';
+import './App.css';
 
 function App() {
   return (
--- admin/src/components/login.jsx
+++ admin/src/components/login.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import login from '../img/login.jpg'
+import login from '../img/login.jpg';
+import './Login.css'
+
 function Login() {
   return (
     <div>
-        Welcome to the Login Page!
-        <img src={login}/>
+        {/* Welcome to the Login Page! */}
+
+        <div className='loginPage-img'>
+            <div className='login-heading'>
+              <h1>Welcome to Login Page</h1>
+            </div>
+        </div>
     </div>
   )
 }
