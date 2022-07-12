3800299ba5b4a1d9362fa8122c87ce2f52cdc7b6
Varzika Tiwari <varzikatiwari08@gmail.com>
2022-07-12 17:27:24 +0530

    done

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,18 @@
-import './App.css';
+import "./App.css";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import { BrowserRouter,
-  Routes,
-  Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
-      <Routes >
-
-        <Route path="/" element={<Welcome/>}/>
-
-        <Route path="/login" element={<Login/>}/>
-
-        <Route path="/home" element={<Home/>}/>
-
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
